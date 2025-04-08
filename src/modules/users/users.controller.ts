d2c4dbec6cc15ed6f59a4d7c2e198cd82c29d9a3
Daniel Tavares <daniel.t.santos.prestserv@petrobras.com.br>
2025-04-08 15:25:02 -0300

    Adicionando swagger

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -13,28 +13,34 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 import { AuthGuard } from '../auth/auth.guard';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Usuários')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Listar usuários' })
   async listAll() {
     return this.usersService.listAll();
   }
 
   @UseGuards(AuthGuard)
   @Get(':id')
+  @ApiOperation({ summary: 'Obter dados de um usuário' })
   async findById(@Param('id', ParseIntPipe) id: number) {
     return this.usersService.findById(id);
   }
 
   @Post()
+  @ApiOperation({ summary: 'Cadastrar usuário' })
   async create(@Body() body: CreateUserDto) {
     return this.usersService.create(body);
   }
 
   @Put(':id')
+  @ApiOperation({ summary: 'Editar usuário' })
   async update(
     @Param('id', ParseIntPipe) id: number,
     @Body() body: UpdateUserDto,
@@ -43,6 +49,7 @@ export class UsersController {
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Excluir usuário' })
   async delete(@Param('id', ParseIntPipe) id: number) {
     return this.usersService.delete(id);
   }
